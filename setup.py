999f1bccef0538f4b65d9275ec7987129608c794
edeno <edeno@bu.edu>
2017-10-11 12:34:06 -0400

    Add setup.py for pip install

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+        name='time_rescale',
+        version='0.1.0',
+        description=('Tools for evaluating the goodness of fit of a point'
+                     'process model via the time rescaling theorem'),
+        author='Eric Denovellis',
+        author_email='edeno@bu.edu',
+        packages=find_packages(),
+        install_requires=['numpy', 'scipy', 'matplotlib'],
+)
