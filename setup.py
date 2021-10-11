e421ba83bd3b24c5a1008f3790aaa197a3e8b2f6
Eric Denovellis <edeno@bu.edu>
2021-10-11 19:28:00 -0400

    Bump version number

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ INSTALL_REQUIRES = ['numpy', 'scipy', 'matplotlib']
 
 setup(
     name='time_rescale',
-    version='0.2.0',
+    version='0.2.1',
     description=('Tools for evaluating the goodness of fit of a point'
                  'process model via the time rescaling theorem'),
     author='Eric Denovellis',
