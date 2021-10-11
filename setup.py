30fcd92a9660ca91687fbdd0299a027e55b8326a
Eric Denovellis <edeno@bu.edu>
2021-10-11 16:51:10 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from setuptools import setup, find_packages
+from setuptools import find_packages, setup
 
 INSTALL_REQUIRES = ['numpy', 'scipy', 'matplotlib']
 
 setup(
-        name='time_rescale',
-        version='0.1.0',
-        description=('Tools for evaluating the goodness of fit of a point'
-                     'process model via the time rescaling theorem'),
-        author='Eric Denovellis',
-        author_email='edeno@bu.edu',
-        packages=find_packages(),
-        url='https://github.com/Eden-Kramer-Lab/time_rescale',
-        license='MIT',
-        install_requires=INSTALL_REQUIRES,
+    name='time_rescale',
+    version='0.2.0',
+    description=('Tools for evaluating the goodness of fit of a point'
+                 'process model via the time rescaling theorem'),
+    author='Eric Denovellis',
+    author_email='eric.denovellis@ucsf.edu',
+    packages=find_packages(),
+    url='https://github.com/Eden-Kramer-Lab/time_rescale',
+    license='MIT',
+    install_requires=INSTALL_REQUIRES,
 )
